43cbdc3722d441fa25498eca50fa69c6dc4f2088
Daniel Andrade <dsandrade777@gmail.com>
2019-07-11 11:35:08 -0300

    Algumas correções no titulo e no favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Portfólio Daniel Andrade',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Portfólio Daniel Andrade'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
